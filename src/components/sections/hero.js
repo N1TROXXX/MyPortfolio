65f9605cc8bccac5493fe49a6cd3d48dcabbcbce
Lasha Alkhazishvili <alkhazishvili2@gmail.com>
2024-11-17 20:38:02 +0100

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,9 +60,9 @@ const Hero = () => {
   }, []);
 
   
-  const one = <h1>Hi, I'm</h1>;
+  const one = <h2>Hi, I'm</h1>;
   const two = <h2 className="big-heading">Lasha Alkhazishvili</h2>;
-  const three = <h3 className="big-heading">Front-End Developer</h3>;
+  const three = <h2 className="big-heading">Front-End Developer</h3>;
   const four = (
     <>
       <p>    
