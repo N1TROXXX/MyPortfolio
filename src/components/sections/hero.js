0dbafa30d5afe92e110d4e649b5cedad4588a53d
Lasha Alkhazishvili <alkhazishvili2@gmail.com>
2024-11-17 19:14:54 +0100

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,13 +59,15 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Lasha.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  
+  const one = <h1>Hi, I'm</h1>;
+  const two = <h2 className="big-heading">Lasha Alkhazishvili</h2>;
+  const three = <h3 className="big-heading">I'm Front-End Developer based in Stockholm, Sweden.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
+    <h1 class="text-[30px] md:text-[50px] lg:text-[70px] lg:font-[900] text-[--textWhite] font-bold">Davit Andzavelidze</h1>    
+    I’m a software engineer specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
