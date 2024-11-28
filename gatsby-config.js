4794d10931a2c044220247fb8743901a5d94e6ac
Lasha Alkhazishvili <alkhazishvili2@gmail.com>
2024-11-28 21:51:24 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Hi, I'm Lasha - Front-End Developer',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'I specialize in building responsive, user-friendly websites, that are both visually appealing and highly functional.',
+    siteUrl: 'https://alkhazishvili.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@alkhazishvili2',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
